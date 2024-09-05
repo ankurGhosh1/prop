0c9b6c281beb3c5f9caaec4ac0b20885595a5a78
ankur-astra <ankur.ghosh@getastra.com>
2024-09-06 02:04:34 +0530

    added GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,53 +1,11 @@
-import { useEffect } from "react";
-import Script from "next/script";
-import { useRouter } from "next/router";
 import "@/styles/globals.css";
 import { NextSeo } from "next-seo";
-
-const GA_TRACKING_ID = "G-7Z44WV13KY";
-
-const trackPageView = (url) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      trackPageView(url);
-    };
-
-    // Listen for route changes and trigger a page view
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Google Analytics script */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <GoogleAnalytics />
       <NextSeo
         title="Best Driving School"
         description="Best Driving Schools"
