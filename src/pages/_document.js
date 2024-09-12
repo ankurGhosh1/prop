b0517f870e7ff160682fb373717a4d55aa4b31f0
ankur-astra <ankur.ghosh@getastra.com>
2024-09-13 03:03:42 +0530

    added Google Search Console

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="lQ-OSWJyy-ntpkuKlEVMY4NL24CXGj-jmgBZ8-2zjSI"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
