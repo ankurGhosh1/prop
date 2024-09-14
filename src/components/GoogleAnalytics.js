bceed4b0e32a64668dd81f6b51454ede780f7fb9
ankur-astra <ankur.ghosh@getastra.com>
2024-09-14 19:54:51 +0530

    removed google analytics for bots

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Script from "next/script";
 
+// Bot detection function
+const isBot = () => {
+  if (typeof window === "undefined") return false; // Server-side rendering (SSR)
+  const botPatterns = [/bot/i, /crawl/i, /spider/i, /slurp/i, /mediapartners/i];
+  return botPatterns.some((pattern) => pattern.test(navigator.userAgent));
+};
+
 const GoogleAnalytics = () => {
+  const [isClientBot, setIsClientBot] = useState(false);
+
+  useEffect(() => {
+    setIsClientBot(isBot());
+  }, []);
+
+  if (isClientBot) {
+    // Do not load Google Analytics if it's a bot
+    return null;
+  }
+
   return (
     <>
       <Script
