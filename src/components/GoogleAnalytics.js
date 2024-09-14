b02a39324a00c32608cc3a00344e91a80761e0dd
ankur-astra <ankur.ghosh@getastra.com>
2024-09-14 20:05:18 +0530

    added navigator to check speed bot

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,31 +1,41 @@
 import React, { useState, useEffect } from "react";
 import Script from "next/script";
 
-const isBotOrSpeedTest = () => {
-  if (typeof window === "undefined") return false;
+const isSpeedTestOrBot = () => {
+  if (typeof window === "undefined") return false; // Return false for SSR
+
+  // Detect connection saver flag (common for PageSpeed tests)
+  const saveData = navigator.connection && navigator.connection.saveData;
+
+  // Detect bot or Google PageSpeed Insights via user-agent
   const botPatterns = [
     /bot/i,
     /crawl/i,
     /spider/i,
     /slurp/i,
     /mediapartners/i,
-    /Lighthouse/i,
-    /Chrome-Lighthouse/i,
+    /Lighthouse/i, // Google Lighthouse
+    /Chrome-Lighthouse/i, // Google Chrome Lighthouse
   ];
-  return botPatterns.some((pattern) => pattern.test(navigator.userAgent));
+
+  const isBot = botPatterns.some((pattern) =>
+    pattern.test(navigator.userAgent)
+  );
+
+  return saveData || isBot; // Return true if saveData is enabled or a bot is detected
 };
 
 const GoogleAnalytics = () => {
   const [shouldLoadGA, setShouldLoadGA] = useState(true);
 
   useEffect(() => {
-    if (isBotOrSpeedTest()) {
-      setShouldLoadGA(false);
+    if (isSpeedTestOrBot()) {
+      setShouldLoadGA(false); // Do not load Google Analytics if it's a bot or speed test
     }
   }, []);
 
   if (!shouldLoadGA) {
-    return null;
+    return null; // Skip loading the GA script
   }
 
   return (
